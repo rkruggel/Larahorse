8f87763f61e5175ff93563053b629d96b6b9d85e
Roland <rkruggel@bbf7.de>
2020-11-28 14:57:39 +0100

    first commit nach copy von mix
    Signed-off-by: Roland <rkruggel@bbf7.de>

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Projekt  Larahorse
+ * ----------------------------------------------------
+ *
+ * Copyright (c) 2020-2020,  Roland Kruggel
+ * All Rights Reserved.
+ * License: MIT
+ *
+ * @author		Roland Kruggel, rkruggel@bbf7.de
+ * @file		ExampleTest.php
+ * @path		/home/roland/Develop/Larahorse/tests/Feature/ExampleTest.php
+ * @lastChange	20.11.20, 10:19 by roland
+ */
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
