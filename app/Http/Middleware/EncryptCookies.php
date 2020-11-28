4ad9861ae6abd661dd606bd70fc4d3e4f222c8f2
Roland <rkruggel@bbf7.de>
2020-11-28 14:55:58 +0100

    first commit nach copy von mix
    Signed-off-by: Roland <rkruggel@bbf7.de>

---
+++ app/Http/Middleware/EncryptCookies.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Projekt  Larahorse
+ * ----------------------------------------------------
+ *
+ * Copyright (c) 2020-2020,  Roland Kruggel
+ * All Rights Reserved.
+ * License: MIT
+ *
+ * @author		Roland Kruggel, rkruggel@bbf7.de
+ * @file		EncryptCookies.php
+ * @path		/home/roland/Develop/Larahorse/app/Http/Middleware/EncryptCookies.php
+ * @lastChange	20.11.20, 10:19 by roland
+ */
+
+namespace App\Http\Middleware;
+
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+
+class EncryptCookies extends Middleware
+{
+    /**
+     * The names of the cookies that should not be encrypted.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
