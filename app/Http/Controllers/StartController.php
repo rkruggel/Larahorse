dda82c3532733a05183909e5f5ab82b3c9e9a656
Roland <rkruggel@bbf7.de>
2021-01-06 15:50:56 +0100

    neues copyright für die einzelnen files

--- app/Http/Controllers/StartController.php
+++ app/Http/Controllers/StartController.php
@@ -3,14 +3,15 @@
  * Projekt  Larahorse
  * ----------------------------------------------------
  *
- * Copyright (c) 2020-2020,  Roland Kruggel
+ * Copyright (c) 2020-2021,  Roland Kruggel
  * All Rights Reserved.
  * License: MIT
  *
  * @author		Roland Kruggel, rkruggel@bbf7.de
  * @file		StartController.php
  * @path		/home/roland/Develop/Larahorse/app/Http/Controllers/StartController.php
- * @lastChange	20.11.20, 10:19 by roland
+ * @lastChange	06.01.21, 15:49 by roland
+ *
  */
 
 namespace App\Http\Controllers;
